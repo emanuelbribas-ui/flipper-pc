e11a7827c34009df1763b504a069efe7625b7ef1
Mysthic_Fox <emanuelbribas@gmail.com>
2026-05-10 00:31:29 -0300

    Criar setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Nebula Studios - Setup de Antena
+echo "Configurando antena..."
+sudo ip link set wlan0 down
+sudo iw dev wlan0 set type monitor
+sudo ip link set wlan0 up
+echo "Modo monitor ativado em wlan0."
